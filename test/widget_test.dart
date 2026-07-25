8c3549e841fbfbf8a43cf483aaa49a0edc63e5c0
jinsungkim <kimswim02@gmail.com>
2026-07-25 14:05:56 +0900

    update:진도 업데이트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:project00/platform/app/app.dart';
 
 void main() {
   testWidgets('빈 앱이 실행된다', (tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(App(authStateChanges: Stream<User?>.value(null)));
     expect(tester.takeException(), isNull);
   });
 }
