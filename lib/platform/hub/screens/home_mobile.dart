ff8aca4d71b292a53a29c3c5712c5c08ff969bbc
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-27 14:51:47 +0900

    feat: 그룹 참여 버튼 생성

--- lib/platform/hub/screens/home_mobile.dart
+++ lib/platform/hub/screens/home_mobile.dart
@@ -15,9 +15,21 @@ class _HomeMobileState extends State<HomeMobile> {
         children: [
           Row(
             children: [
+              SizedBox(height: 100),
               TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.grey[300],
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50.0,
+                    vertical: 15.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
                 onPressed: () {
-                  print("그룹에 참여하기");
+                  print("그룹에 참여하기"); // 나중에 클릭 시 그룹 참여하기 카메라 큐알 페이지로 넘기기
                 },
                 child: Text("그룹 참여"),
               ),
