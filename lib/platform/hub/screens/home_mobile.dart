f3516effd758180b636d40b82a328edf5ede4d44
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-27 11:35:09 +0900

    feat: 기본 구성

--- lib/platform/hub/screens/home_mobile.dart
+++ lib/platform/hub/screens/home_mobile.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class HomeMobile extends StatefulWidget {
+  const HomeMobile({super.key});
+
+  @override
+  State<HomeMobile> createState() => _HomeMobileState();
+}
+
+class _HomeMobileState extends State<HomeMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
