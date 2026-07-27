9118822d892bb1014d3280eaf262fc2becc44f8c
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-27 17:10:35 +0900

    feat: 보유 중인 게임 텍스트 배치 완료

--- lib/platform/hub/screens/home_mobile.dart
+++ lib/platform/hub/screens/home_mobile.dart
@@ -11,56 +11,78 @@ class _HomeMobileState extends State<HomeMobile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(height: 100),
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.grey[300],
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 40.0,
-                    vertical: 10.0,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.grey[300],
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40.0,
+                      vertical: 10.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
                   ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
+                  onPressed: () {
+                    print("그룹에 참여하기"); // 나중에 클릭 시 그룹 참여하기 카메라 큐알 페이지로 넘기기
+                  },
+                  child: Text(
+                    "그룹 참여",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      letterSpacing: -0.5,
+                    ),
                   ),
                 ),
-                onPressed: () {
-                  print("그룹에 참여하기"); // 나중에 클릭 시 그룹 참여하기 카메라 큐알 페이지로 넘기기
-                },
-                child: Text(
-                  "그룹 참여",
+                Ink(
+                  width: 50,
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage("https://picsum.photos/600/400"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: InkWell(
+                    customBorder: CircleBorder(),
+                    onTap: () {
+                      print('click');
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 10),
+                Text(
+                  "보유 중인 게임",
                   style: TextStyle(
-                    fontSize: 20.0,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Colors.black87,
-                    letterSpacing: -0.5,
                   ),
                 ),
-              ),
-              Ink(
-                width: 50,
-                height: 50,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: NetworkImage("https://picsum.photos/600/400"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: InkWell(
-                  customBorder: CircleBorder(),
-                  onTap: () {
-                    print('click');
-                  },
-                ),
-              ),
-            ],
-          ),
-        ],
+                SizedBox(height: 10),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
