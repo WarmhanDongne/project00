747d69802542563447cea5f3e2044c82f7c3e1b5
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-29 11:31:30 +0900

    feat: game_service 추가,

--- lib/platform/hub/screens/home_mobile.dart
+++ lib/platform/hub/screens/home_mobile.dart
@@ -155,28 +155,30 @@ class MobileGameCard extends StatelessWidget {
               ),
             ),
             SizedBox(width: 20),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              //crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  gameInfo.title, // 텍스트 대신 DTO로 받은 데이터를 디스플레이
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
-                  textScaler: TextScaler.noScaling,
-                ),
-                Text(
-                  gameInfo.playTime,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
-                ),
-                Text(
-                  gameInfo.userCount,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
-                ),
-                Text(
-                  gameInfo.shortExplain,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
-                ),
-              ],
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    gameInfo.title, // 텍스트 대신 DTO로 받은 데이터를 디스플레이
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
+                    textScaler: TextScaler.noScaling,
+                  ),
+                  Text(
+                    '${gameInfo.playTime}',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
+                  ),
+                  Text(
+                    '${gameInfo.minPlayers} ~ ${gameInfo.maxPlayers}인',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
+                  ),
+                  Text(
+                    gameInfo.shortDescription,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight(400)),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -189,26 +191,30 @@ class MobileGameCard extends StatelessWidget {
 // DTO 클래스 선언
 class GameInfo {
   final String title;
-  final String playTime;
-  final String userCount;
-  final String gameGenre;
-  final String shortExplain;
+  final int playTime;
+  final int minPlayers;
+  final int maxPlayers;
+  final List<String> genres;
+  final String shortDescription;
 
   GameInfo({
     required this.title,
     required this.playTime,
-    required this.userCount,
-    required this.gameGenre,
-    required this.shortExplain,
+    required this.minPlayers,
+    required this.maxPlayers,
+    required this.genres,
+    required this.shortDescription,
   });
 
   factory GameInfo.fromJson(Map<String, dynamic> json) {
+    print('$json');
     return GameInfo(
       title: json['name'] as String? ?? '이름 없음',
-      playTime: json['playTime'] as String? ?? '미상',
-      userCount: json['userCount'] as String? ?? '장르 없음',
-      gameGenre: json['genre'] as String? ?? '장르 없음',
-      shortExplain: json['shortExplain'] as String? ?? '설명 없음',
+      playTime: (json['playTimeMin'] as num?)?.toInt() ?? 0,
+      minPlayers: (json['minPlayers'] as int?)?.toInt() ?? 0,
+      maxPlayers: json['maxPlayers'] as int? ?? 0,
+      genres: json['genres'] != null ? List<String>.from(json['genres']) : [],
+      shortDescription: json['shortDescription'] as String? ?? '설명 없음',
     );
   }
 }
