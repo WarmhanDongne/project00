af7f7439e51806979cccf231630889eb257420ea
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-29 16:10:56 +0900

    feat: 위젯 감싸기 수정

--- lib/platform/app/app.dart
+++ lib/platform/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:project00/platform/auth/screens/login_screen.dart';
 import 'package:project00/platform/auth/screens/register_screen.dart';
@@ -13,38 +12,36 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Project 00',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Project 00',
 
-        home: StreamBuilder<User?>(
-          stream: authStateChanges ?? FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            // Firebase 로그인 상태 확인 중
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Scaffold(
-                body: Center(child: CircularProgressIndicator()),
-              );
+      home: StreamBuilder<User?>(
+        stream: authStateChanges ?? FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // Firebase 로그인 상태 확인 중
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          // 로그인된 사용자
+          if (snapshot.hasData && snapshot.data != null) {
+            final user = snapshot.data!;
+
+            // Google 로그인 후 필수 정보가 없다면 회원가입 계속 진행
+            if (user.phoneNumber == null || user.displayName == null) {
+              return const RegisterScreen(isGoogleSignIn: true);
             }
 
-            // 로그인된 사용자
-            if (snapshot.hasData && snapshot.data != null) {
-              final user = snapshot.data!;
+            // 로그인 완료 → 홈 화면
+            return const Home();
+          }
 
-              // Google 로그인 후 필수 정보가 없다면 회원가입 계속 진행
-              if (user.phoneNumber == null || user.displayName == null) {
-                return const RegisterScreen(isGoogleSignIn: true);
-              }
-
-              // 로그인 완료 → 홈 화면
-              return const Home();
-            }
-
-            // 로그인 안 된 경우
-            return const LoginScreen();
-          },
-        ),
+          // 로그인 안 된 경우
+          return const LoginScreen();
+        },
       ),
     );
   }
