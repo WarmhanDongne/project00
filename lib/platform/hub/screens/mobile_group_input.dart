f57c84f06933b3cdc3bd439b8aded0a5099d03cd
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-29 20:09:13 +0900

    feat: 참여자 배치

--- lib/platform/hub/screens/mobile_group_input.dart
+++ lib/platform/hub/screens/mobile_group_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:project00/platform/app/app.dart';
 
 class MobileGroupInput extends StatefulWidget {
   const MobileGroupInput({super.key});
@@ -10,6 +12,29 @@ class MobileGroupInput extends StatefulWidget {
 class _MobileGroupInputState extends State<MobileGroupInput> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.grey);
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text("그룹 참여하기")),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 46.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "참여자 [ 4명 ]",
+                  style: TextStyle(
+                    fontSize: 25.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ), // n명 데이터 받아서 해야함.
+                SizedBox(height: 10.h),
+                Container(height: 310.h, width: 272.w, color: Colors.grey),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
