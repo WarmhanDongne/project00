91df51b2ba0fe8504b550bb18456cc9e454f880c
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-29 17:14:26 +0900

    feat: 화면 비율에 따른 .w, .h 설정

--- lib/platform/hub/screens/mobile_group_join.dart
+++ lib/platform/hub/screens/mobile_group_join.dart
@@ -3,6 +3,7 @@
 카메라를 통한 큐알 스캔 또는 참여 코드로 그룹에 입장하는 단계의 페이지.
 */
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:project00/platform/hub/screens/mobile_group_input.dart';
 
 class MobileGroupJoin extends StatefulWidget {
@@ -13,57 +14,79 @@ class MobileGroupJoin extends StatefulWidget {
 }
 
 class _MobileGroupJoinState extends State<MobileGroupJoin> {
-  late double screenWidth = MediaQuery.sizeOf(context).width; // 화면 가로 너비
-  late double screenHeight = MediaQuery.sizeOf(context).height; // 화면 새로 높이
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(centerTitle: true, title: Text("그룹 참여하기")),
-        body: Column(
-          children: [
-            SizedBox(height: 4),
-            Padding(
-              padding: EdgeInsetsGeometry.symmetric(horizontal: 23),
-              child: Text(
-                "테블릿에 표시된 QR 코드를 스캔, 혹은 참여 코드를 입력해 주세요. ",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight(400), fontSize: 25),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              SizedBox(height: 4.h),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 23.w),
+                child: Text(
+                  "테블릿에 표시된 QR 코드를 스캔, 혹은 참여 코드를 입력해 주세요. ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 25.sp,
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 26),
-            Container(
-              height: screenHeight * 0.4,
-              width: screenHeight * 0.4,
-              color: Colors.grey,
-              child: Center(child: Text('카메라')),
-            ),
-            SizedBox(height: 36),
-            Text(
-              "--------------- OR --------------",
-              style: TextStyle(fontWeight: FontWeight(400), fontSize: 25),
-            ),
-            SizedBox(height: 26),
-            Container(
-              height: 126,
-              width: 342,
-              color: Colors.grey,
-              child: Center(
-                child: Text('_______ _______ _______ _______ _______ ________'),
+              SizedBox(height: 26.h),
+              Container(
+                height: 310.h,
+                width: 310.w,
+                color: Colors.grey,
+                child: Center(child: Text('카메라')),
               ),
-            ),
-            //SizedBox(height: 26),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MobileGroupInput()),
-                );
-              },
-              child: Text('data'),
-            ),
-          ],
+              SizedBox(height: 36.h),
+              Text(
+                "--------------- OR --------------",
+                style: TextStyle(fontWeight: FontWeight(400), fontSize: 25.sp),
+              ),
+              SizedBox(height: 26.h),
+              Container(
+                height: 126.h,
+                width: 342.w,
+                color: Colors.grey,
+                child: Center(
+                  child: Text(
+                    '_______ _______ _______ _______ _______ ________',
+                  ),
+                ),
+              ),
+              SizedBox(height: 36.h),
+              SizedBox(
+                width: 164.w,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.grey,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.w,
+                      vertical: 10.h,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0.r),
+                    ),
+                  ),
+
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MobileGroupInput(),
+                      ),
+                    );
+                  },
+                  child: Text('입력 완료'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
