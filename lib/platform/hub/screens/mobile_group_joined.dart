0986db83f4dcf29cbb3dd098cca2d115fb5ab176
dongdong7-max <dongdong7@handong.ac.kr>
2026-07-31 15:57:01 +0900

    feat: create mobile_group_joined

---
+++ lib/platform/hub/screens/mobile_group_joined.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileGroupJoined extends StatefulWidget {
+  const MobileGroupJoined({super.key});
+
+  @override
+  State<MobileGroupJoined> createState() => _MobileGroupJoinedState();
+}
+
+class _MobileGroupJoinedState extends State<MobileGroupJoined> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
