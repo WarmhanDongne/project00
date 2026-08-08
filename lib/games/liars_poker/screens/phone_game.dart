62bd80b5283e9a0e17ff35ab9dffdc8c09d153cf
dongdong7-max <dongdong7@handong.ac.kr>
2026-08-08 11:49:55 +0900

    feat: 핸드폰 UI 뼈대 코드

--- lib/games/liars_poker/screens/phone_game.dart
+++ lib/games/liars_poker/screens/phone_game.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:project00/games/liars_poker/models/player_layout_model.dart';
+import 'package:project00/games/liars_poker/widgets/phone/hand_card_stack.dart';
 
-class PhoneGame extends StatelessWidget {
+class PhoneGame extends StatefulWidget {
   const PhoneGame({super.key, required this.playerLayout});
 
   final PlayerLayoutModel playerLayout;
 
   @override
+  State<PhoneGame> createState() => _PhoneGameState();
+}
+
+class _PhoneGameState extends State<PhoneGame> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/games/liars_poker/images/background/phone_background.png',
+              fit: BoxFit.cover,
+              filterQuality: FilterQuality.high,
+            ),
+          ),
+
+          const Positioned(
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: Center(child: HandCardStack()),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/games/liars_poker/widgets/phone/hand_card_stack.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HandCardStack extends StatelessWidget {
+  const HandCardStack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
