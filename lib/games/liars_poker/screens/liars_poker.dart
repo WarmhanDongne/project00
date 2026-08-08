698e67ddb136997d97efc6ab5058e2861d94161f
dongdong7-max <dongdong7@handong.ac.kr>
2026-08-08 23:40:32 +0900

    feat: 라이어스 포커 핸드폰 테블릿 분기 파일을 PhoneGame으로 수정.

--- lib/games/liars_poker/screens/liars_poker.dart
+++ lib/games/liars_poker/screens/liars_poker.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project00/games/liars_poker/models/player_layout_model.dart';
-import 'package:project00/games/liars_poker/screens/phone_game_portrait.dart';
+import 'package:project00/games/liars_poker/screens/phone_game.dart';
 import 'package:project00/games/liars_poker/screens/tablet_game.dart';
 import 'package:project00/games/liars_poker/services/liars_poker_service.dart';
 import 'package:project00/platform/home/room/providers/room_provider.dart';
@@ -25,7 +25,7 @@ class LiarsPoker extends StatelessWidget {
       builder: (context, constraints) {
         // 모바일 브레이크포인트 설정 (일반적으로 600px 기준)
         if (constraints.maxWidth < 600 || constraints.maxHeight < 900) {
-          return PhoneGamePortrait(
+          return PhoneGame(
             //playerLayout: playerLayout
           );
         } else {
